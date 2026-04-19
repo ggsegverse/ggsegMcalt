ggsegverse/ggsegMars

Add cerebellar atlas output to wholebrain pipeline
Update make_atlas.R to capture cerebellar output from
create_wholebrain_from_volume() and add cerebellar accessor
function to R/data.R.

Co-Authored-By: Claude Opus 4.6 (1M context) <noreply@anthropic.com>

data-raw/make_atlas.R
@@ -37,11 +37,19 @@ atlases <- create_wholebrain_from_volume(
 
 .marsatlas_cortical <- atlases$cortical
 .marsatlas_subcortical <- atlases$subcortical
+.marsatlas_cerebellar <- atlases$cerebellar
 
 print(.marsatlas_cortical)
 plot(.marsatlas_cortical)
 print(.marsatlas_subcortical)
 plot(.marsatlas_subcortical)
 
-usethis::use_data(.marsatlas_cortical, .marsatlas_subcortical,
-  overwrite = TRUE, compress = "xz", internal = TRUE)
+if (!is.null(.marsatlas_cerebellar)) {
+  print(.marsatlas_cerebellar)
+  plot(.marsatlas_cerebellar)
+  usethis::use_data(.marsatlas_cortical, .marsatlas_subcortical,
+    .marsatlas_cerebellar, overwrite = TRUE, compress = "xz", internal = TRUE)
+} else {
+  usethis::use_data(.marsatlas_cortical, .marsatlas_subcortical,
+    overwrite = TRUE, compress = "xz", internal = TRUE)
+}

ggsegverse/ggsegMcalt

data-raw/make_atlas.R
@@ -21,11 +21,19 @@ atlases <- create_wholebrain_from_volume(
 
 .mcalt_cortical <- atlases$cortical
 .mcalt_subcortical <- atlases$subcortical
+.mcalt_cerebellar <- atlases$cerebellar
 
 print(.mcalt_cortical)
 plot(.mcalt_cortical)
 print(.mcalt_subcortical)
 plot(.mcalt_subcortical)
 
-usethis::use_data(.mcalt_cortical, .mcalt_subcortical,
-  overwrite = TRUE, compress = "xz", internal = TRUE)
+if (!is.null(.mcalt_cerebellar)) {
+  print(.mcalt_cerebellar)
+  plot(.mcalt_cerebellar)
+  usethis::use_data(.mcalt_cortical, .mcalt_subcortical,
+    .mcalt_cerebellar, overwrite = TRUE, compress = "xz", internal = TRUE)
+} else {
+  usethis::use_data(.mcalt_cortical, .mcalt_subcortical,
+    overwrite = TRUE, compress = "xz", internal = TRUE)
+}